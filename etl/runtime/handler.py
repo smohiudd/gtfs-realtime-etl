8500d2b899784b2c87dbd44380814355c0ccdb35
smohiudd <saadiq@developmentseed.org>
2025-10-09 08:38:55 -0600

    update etl handler

--- etl/runtime/handler.py
+++ etl/runtime/handler.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import json
 
 import boto3
 from botocore.exceptions import ClientError
@@ -10,8 +11,8 @@ from zoneinfo import ZoneInfo
 
 import pandas as pd
 import pyarrow as pa
+from pyarrow import parquet
 import geoarrow.pyarrow as ga
-from geoarrow.pyarrow import io
 
 import datetime as dt
 
@@ -41,41 +42,98 @@ def handler(event, context):
 
     feed.ParseFromString(response.content)
 
-    records = [
-        (
-            i.vehicle.trip.trip_id,
-            dt.datetime.fromtimestamp(
-                i.vehicle.timestamp, tz=ZoneInfo(timezone)
-            ).isoformat(),
-            i.vehicle.position.longitude,
-            i.vehicle.position.latitude,
-        )
-        for i in feed.entity
-    ]
+    records = []
+    for entity in feed.entity:
+        if entity.HasField("vehicle"):
+            v = entity.vehicle
+            records.append(
+                {
+                    # tripid
+                    "trip_id": v.trip.trip_id if v.HasField("trip") else None,
+                    "route_id": v.trip.route_id if v.HasField("trip") else None,
+                    "direction_id": v.trip.route_id if v.HasField("trip") else None,
+                    # vehicle
+                    "vehicle_id": v.vehicle.id if v.HasField("vehicle") else None,
+                    # position
+                    "latitude": v.position.latitude if v.HasField("position") else None,
+                    "longitude": v.position.longitude
+                    if v.HasField("position")
+                    else None,
+                    "bearing": v.position.bearing if v.HasField("position") else None,
+                    "speed": v.position.speed if v.HasField("position") else None,
+                    # timestamp
+                    "timestamp": dt.datetime.fromtimestamp(
+                        v.timestamp, tz=ZoneInfo(timezone)
+                    ).isoformat()
+                    if v.HasField("timestamp")
+                    else None,
+                }
+            )
 
     logger.info(f"Discovered {len(records)} vehicle position records")
 
-    columns = ["trip_id", "datetime", "lon", "lat"]
-    df = pd.DataFrame(records, columns=columns)
+    df = pd.DataFrame(records)
+    df = df.replace(0.0, pd.NA)
+    df = df.replace("", pd.NA)
+    dtypes_mapping = {
+        "trip_id": "string",
+        "route_id": "string",
+        "direction_id": "string",
+        "vehicle_id": "string",
+        "latitude": "Float64",
+        "longitude": "Float64",
+        "bearing": "Float64",
+        "speed": "Float64",
+        "timestamp": pd.DatetimeTZDtype(unit="ns", tz=timezone),
+    }
 
-    df["geometry_wkt"] = df.apply(lambda x: f"POINT ({x['lon']} {x['lat']})", axis=1)
+    df = df.astype(dtypes_mapping)
 
-    pa_table = pa.Table.from_pandas(df, preserve_index=False)
-    geom_array = ga.as_geoarrow(
-        ga.with_crs(pa_table["geometry_wkt"].combine_chunks(), ga.OGC_CRS84)
+    geo_meta = {
+        "version": "1.1.0",
+        "primary_column": "geometry",
+        "columns": {
+            "geometry": {
+                "encoding": "WKB",
+                "geometry_types": ["Point"],
+                "crs": "EPSG:4326",
+            }
+        },
+    }
+
+    schema = pa.schema(
+        [
+            pa.field("trip_id", pa.string()),
+            pa.field("route_id", pa.string()),
+            pa.field("direction_id", pa.string()),
+            pa.field("vehicle_id", pa.string()),
+            pa.field("latitude", pa.float64()),
+            pa.field("longitude", pa.float64()),
+            pa.field("bearing", pa.float64()),
+            pa.field("speed", pa.float64()),
+            pa.field("timestamp", pa.timestamp("s", tz=timezone)),
+        ],
     )
 
-    pa_table = pa_table.append_column("geometry", geom_array)
-    pa_table = pa_table.drop_columns(["lon", "lat", "geometry_wkt"])
+    schema = schema.with_metadata({"geo": json.dumps(geo_meta)})
+
+    pa_table = pa.Table.from_pandas(df, preserve_index=False, schema=schema)
+
+    geom_field = pa.field(
+        "geometry", ga.wkb(), metadata={b"ARROW:extension:name": b"geoarrow.wkb"}
+    )
+
+    points = ga.point().from_geobuffers(
+        None, df["longitude"].to_numpy(), df["latitude"].to_numpy()
+    )
+
+    wkb_array = ga.as_wkb(ga.with_crs(points, ga.OGC_CRS84))
+
+    pa_table = pa_table.append_column(geom_field, wkb_array)
 
     output_file = "/tmp/positions.parquet"
 
-    io.write_geoparquet_table(
-        pa_table,
-        output_file,
-        geometry_encoding="point",
-        primary_geometry_column="geometry",
-    )
+    parquet.write_table(pa_table, output_file)
 
     logger.info(f"saved parquet file to {output_file}")
 
